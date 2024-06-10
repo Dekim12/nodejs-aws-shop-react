8e83ee5ab1404ba701e825041c989e6e4bdeede3
Siarhei Zykau <zykau_s@mail.ru>
2024-06-10 10:49:00 +0200

    feat: add ability to deploy app with aws cdk

---
+++ aws_deployment_ci/test/aws_deployment_ci.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsDeploymentCi = require('../lib/aws_deployment_ci-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_deployment_ci-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsDeploymentCi.AwsDeploymentCiStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
